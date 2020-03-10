69a586e1af2d55f15bea30a26eb9ec594808d97f
Suvash Budhathoki <SuvashBudhathoki@users.noreply.github.com>
2020-03-10 14:24:33 +1000

    Updates

---
+++ precache-manifest.6acf3b6b146406836fd8b1c0ff24928b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a2da789301f2faf3eeca60b2673ec5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "066250a2675140a7b5e1",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "ef1c77ab672583c4bca8",
+    "url": "/static/css/main.5d257e1a.chunk.css"
+  },
+  {
+    "revision": "066250a2675140a7b5e1",
+    "url": "/static/js/2.8ab37bff.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/static/js/2.8ab37bff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef1c77ab672583c4bca8",
+    "url": "/static/js/main.7aee3d26.chunk.js"
+  },
+  {
+    "revision": "7f31f04669a7e0c04c51",
+    "url": "/static/js/runtime-main.59d46d1a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
